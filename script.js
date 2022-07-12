8be8766aa821b34dbe6cc34341304110e4c3a3c7
TortaXx <tortaxxcz@gmail.com>
2022-07-12 20:00:52 +0200

    Implement basic js functionality

--- script.js
+++ script.js
@@ -1,61 +1,58 @@
-function chooseWinner(player, computer) {
-    if (computer === player) {
-        return 0;
-    }
-    if (player === 0) {
-        return (computer === 1) ? 1 : -1;
-    }
-    if (player === 1) {
-        return (computer === 2) ? 1 : -1;
-    }
-    return (computer == 0) ? 1 : -1;
-}
+let playerButtons = document.querySelectorAll(".pick.player");
 
-function showPick(name, pickNum) {
-    let pick = "rock";
-    if (pickNum === 1) {
-        pick = "paper";
-    } else if (pickNum === 2) {
-        pick = "scissors";
-    }
-    alert(`${name} chose ${pick}`);
-}
 
-function singleRound() {
-    let player = Number(prompt("Enter 0 for rock, 1 for paper and 2 for scissors"));
-    if (player === NaN || player < 0 || player > 2) {
-        alert("Please enter allowed numbers only");
-        return false;
-    }
-    let computer = Math.floor(Math.random() * 3);
-    showPick("Player", player);
-    showPick("Computer", computer);
-    
-    let result = chooseWinner(player, computer);
-    switch (result) {
-        case -1:
-            alert("Player won");
-            return true;
-        case 0:
-            alert("Draw");
-            return true;
-        case 1:
-            alert("Computer won");
-            return true;
-    }   
-}
 
-function game() {
-    let count = NaN;
-    while (isNaN(count)) {
-        count = Number(prompt("How many games would you like to play?"));
-        if (isNaN(count)) {
-            alert("Enter numbers only");
+playerButtons.forEach( btn => {
+    btn.addEventListener("click", event => {
+        removeAllCurrent();
+        gameLoop(event, btn);
+    });
+});
+
+function removeAllCurrent() {
+    let buttons = document.querySelectorAll(".pick");
+    for (const button of buttons) {
+        if (button.classList.contains("current")) {
+            button.classList.remove("current");
         }
     }
-    for (let i = 0; i < count; i++) {
-        singleRound();
+}
+
+function gameLoop(event, playerBtn) {
+    let playerPick = playerBtn.getAttribute("data-pick");
+    let computerPick = computerChoose();
+    const computerBtn = document.querySelector(`.pick.computer[data-pick="${computerPick}"]`);
+    playerBtn.classList.add("current");
+    computerBtn.classList.add("current");
+
+    let winner = chooseWinner(playerPick, computerPick);
+    if (winner !== "draw") {
+        increaseScore(winner);
     }
 }
 
-game()
+function increaseScore(target) {
+    const scoreText = document.querySelector(`.card.${target} h5`);
+    let score = parseInt(scoreText.textContent.match(/\d+/)[0]); 
+    scoreText.textContent = `Score: ${score + 1}`;
+}
+
+
+function computerChoose() {
+    const picks = ["rock", "paper", "scissors"];
+    let index = Math.floor((Math.random() * 3));
+    return picks[index];
+}
+
+function chooseWinner(player, computer) {
+    if (computer === player) {
+        return "draw";
+    }
+    if (player === "rock") {
+        return (computer === "paper") ? "computer" : "player";
+    }
+    if (player === "paper") {
+        return (computer === "scissors") ? "computer" : "player";
+    }
+    return (computer == "rock") ? "computer" : "player";
+}
